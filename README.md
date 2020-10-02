4e9156f0267ded2e545582923c667ad192d06f8a
AmirrezaFiroozi <firoozi128.af@gmail.com>
2020-10-02 17:55:54 +0330

    Initial Commit. Beta version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # AlaaTV-link-generator
 A simple python program to get the links of all individual videos of a given Alaatv.com course
+
+## This program is still in beta
+
+# Usage
+`echo 'COURSE_LINK' | python3 alaatv_lg.py`
+or run the script first and enter the link
+
+# TODO
+1. handle command line arguments and retrieve the link from the command line
+2. add --help and --separator options
+3. and more ...
---
+++ alaatv_lg.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+# Written By Amirreza Firoozi under the license of GPLv3+
+# For more information refer to the license file you've received with this file
+
+import sys
+import requests
+import re
+import json
+from bs4 import BeautifulSoup
+
+SEPARATOR = '\n'
+
+course_link = input('')
+if course_link == '':
+    print('No Valid link entered. Exitting...')
+    sys.exit(1)
+try:
+    page = requests.get(course_link)
+except:
+    print(
+        f'cannot GET url {course_link}. Valid URL scheme: https://alaatv.com/set/000')
+    sys.exit(1)
+
+if page.status_code != 200:
+    print(
+        f'Error {page.status_code} while fetching url {course_link}. Exitting...')
+    sys.exit(2)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+for script_tag in soup.find_all('script'):
+    script_tag = str(script_tag)
+    if 'var videos' not in script_tag:
+        continue
+    thumbnail_links = re.findall("photo: ('.+?')", script_tag)
+    for thumbnail_link in thumbnail_links:
+        parts = re.findall('/([0-9]+/.+).jpg', thumbnail_link)[0].split('/')
+        media_link = f'https://cdn.alaatv.com/media/{parts[0]}/hq/{parts[1]}.mp4?download=1'
+        print(media_link, end=SEPARATOR)
